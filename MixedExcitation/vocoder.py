7d6b7eaedb494e7a410f9516cc03db58cf506920
ArkoBanik <banik2@illinois.edu>
2018-09-15 16:56:29 -0500

    frame work for mp

---
+++ MixedExcitation/vocoder.py
@@ -0,0 +1,17 @@
+import numpy as np
+from scipy.io import wavfile
+import matplotlib.pyplot as plt
+
+fs, data = wavfile.read('s5.wav')
+#print(fs)
+fp = 1/fs
+
+def analysis(Tframe,Tskip):
+	# frames = np.array()
+
+
+
+
+def synthesis():
+
+
