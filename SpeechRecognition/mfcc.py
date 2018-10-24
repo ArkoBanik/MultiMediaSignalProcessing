5394deea52fc20104bc405ff5df4df0c7f7bd58c
kouarasteh <kouarasteh@gmail.com>
2018-10-24 07:04:27 -0500

    self recordings

---
+++ SpeechRecognition/mfcc.py
@@ -0,0 +1,19 @@
+import python_speech_features as psf
+import math
+import scipy.io.wavfile as wav
+import numpy as np
+
+names = ['asr','cnn','dnn','hmm','tts']
+p = 'feature/self_recf/'
+for n in names:
+    for i in range(1,6):
+        filepath = p+n+str(i)+".fea"
+        print(filepath)
+        f = open(filepath,"w+")
+        (rate,sig) = wav.read("self_rec/"+n+str(i)+".wav")
+        numr = round(rate*0.03)
+        tr = psf.mfcc(sig, samplerate=rate, winlen=0.03, winstep=0.02, numcep=14, nfft=numr, nfilt=40, appendEnergy=True)
+        for t in tr:
+            f.write(",".join([str(x) for x in t]))
+            f.write("\n")
+# print(tr)
