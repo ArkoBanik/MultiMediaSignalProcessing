c7d1105e952f0cba46773755c1f0eef873b203dd
Adithya <adithya.bellary@gmail.com>
2018-10-24 23:26:12 -0500

    added explanations

--- SpeechRecognition/mfcc.py
+++ SpeechRecognition/mfcc.py
@@ -2,18 +2,25 @@ import python_speech_features as psf
 import math
 import scipy.io.wavfile as wav
 import numpy as np
+import os
 
-names = ['asr','cnn','dnn','hmm','tts']
-p = 'feature/self_recf/'
-for n in names:
-    for i in range(1,6):
-        filepath = p+n+str(i)+".fea"
-        print(filepath)
-        f = open(filepath,"w+")
-        (rate,sig) = wav.read("self_rec/"+n+str(i)+".wav")
-        numr = round(rate*0.03)
-        tr = psf.mfcc(sig, samplerate=rate, winlen=0.03, winstep=0.02, numcep=14, nfft=numr, nfilt=40, appendEnergy=True)
-        for t in tr:
-            f.write(",".join([str(x) for x in t]))
-            f.write("\n")
-# print(tr)
+# names = ['asr','cnn','dnn','hmm','tts']
+# p = 'feature/self_recf/'
+# for n in names:
+#     for i in range(1,6):
+#         filepath = p+n+str(i)+".fea"
+#         # print(filepath)
+#         f = open(filepath,"w+")
+#         (rate,sig) = wav.read("self_rec/"+n+str(i)+".wav")
+#         numr = round(rate*0.03)
+#         tr = psf.mfcc(sig, samplerate=rate, winlen=0.03, winstep=0.02, numcep=14, nfft=numr, nfilt=40, appendEnergy=True)
+#         for t in tr:
+#             f.write(",".join([str(x) for x in t]))
+#             f.write("\n")
+
+path = 'feature/'
+dirList = ['dg', 'ls', 'mh', 'yx']
+
+for d in os.listdir(path):
+    if d in dirList:
+        
