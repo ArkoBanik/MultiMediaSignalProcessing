67e00d6a3aa4dc6426f722f0c1995460fc03bc24
ArkoBanik <banik2@illinois.edu>
2018-10-24 20:30:09 -0500

    added python generated mfcc features from input data

--- SpeechRecognition/mfcc.py
+++ SpeechRecognition/mfcc.py
@@ -4,13 +4,13 @@ import scipy.io.wavfile as wav
 import numpy as np
 
 names = ['asr','cnn','dnn','hmm','tts']
-p = 'feature/self_recf/'
+p = 'feature/py_yx/yx_'
 for n in names:
     for i in range(1,6):
         filepath = p+n+str(i)+".fea"
         print(filepath)
         f = open(filepath,"w+")
-        (rate,sig) = wav.read("self_rec/"+n+str(i)+".wav")
+        (rate,sig) = wav.read("data/yx/yx_"+n+str(i)+".wav")
         numr = round(rate*0.03)
         tr = psf.mfcc(sig, samplerate=rate, winlen=0.03, winstep=0.02, numcep=14, nfft=numr, nfilt=40, appendEnergy=True)
         for t in tr:
